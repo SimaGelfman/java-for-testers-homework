03eb9e88df3411e9a98e51bc94d532ded04f37b6
SimaGelfman <GelfmanSima@gmail.com>
2015-09-16 19:49:37 +0300

    Create method modidySomeGroup
    Create method modidySomeGroup

--- addressBookSeleniumTests/src/com/example/tests/GroupModificationTests.java
+++ addressBookSeleniumTests/src/com/example/tests/GroupModificationTests.java
@@ -1,5 +1,20 @@
 package com.example.tests;
 
-public class GroupModificationTests {
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase{
+	
+@Test
+	public void modifySomeGroup(){
+		app.getNavigationHelper().goToMainPage();
+		app.getNavigationHelper().openGroupsPage();
+		app.getGroupHelper().initGroupModification(1);
+		GroupData group;
+		group.name = "new name";
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupModification();
+		app.getNavigationHelper().openGroupsPage();
+
+	}
 
 }
